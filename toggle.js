c3ad11cf869e61ab6277f4cf119a116304970a11
Tayyab Ahmed <tayyabahmed7561@gmail.com>
2026-08-07 10:08:07 +0500

    close menu bar icon pending

--- toggle.js
+++ toggle.js
@@ -1,8 +1,9 @@
 // toggle logic
 
 let menu_icon = document.getElementById('menu-icon');
+// let close_icon = document.getElementById('close-icon');
 let links = document.getElementById('links');
 
 menu_icon.addEventListener("click",()=>{
-    links.classList.toggle('active')
+    let check = links.classList.toggle('active')
 })
