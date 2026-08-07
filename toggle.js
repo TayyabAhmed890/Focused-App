4c44ecc229f99ca06f2a24ffae372327a6085df7
Tayyab Ahmed <tayyabahmed7561@gmail.com>
2026-08-07 10:25:59 +0500

    write toggle logic more closely

--- toggle.js
+++ toggle.js
@@ -1,9 +1,18 @@
 // toggle logic
 
 let menu_icon = document.getElementById('menu-icon');
-// let close_icon = document.getElementById('close-icon');
+let icon_img = document.getElementById('icon-image')
 let links = document.getElementById('links');
 
+console.log(icon_img.src)
 menu_icon.addEventListener("click",()=>{
+    
     let check = links.classList.toggle('active')
+    
+    if(check){
+        icon_img.src = 'close.png'
+    }else{
+        icon_img.src = 'menus.png'
+    }
+
 })
